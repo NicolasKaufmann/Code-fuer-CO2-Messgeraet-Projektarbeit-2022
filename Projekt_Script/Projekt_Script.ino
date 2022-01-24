913b0f6c427dc1fd32d28d1de418ae9cf228c68b
NicolasKaufmann <nicolas.kaufmann@pop.agri.ch>
2022-01-24 17:22:58 +0100

    Projekt_Script gekürzt
    Gekürzte, aber funktionsfähige Version des Projekt_Script.

--- Projekt_Script/Projekt_Script.ino
+++ Projekt_Script/Projekt_Script.ino
@@ -16,10 +16,8 @@ const char* ssid = "WLAN_NAME"; //Name des drahtlosen Netzwerks
 const char* password = "WLAN_PASSWORT"; //Passwort des Netzwerks
 
 //ThingSpeak Setup
-const char* writeAPIKey = "WRITE_API_KEY"; //Key um Daten auf ThingSpeak hochzuladen
+const char* writeAPIkey = "WRITE_API_KEY"; //Key um Daten auf ThingSpeak hochzuladen
 const long int channelID = KANAL_ID; //ID des ThingSpeak Kanals
-
-
 
 void setup() {
   serial.begin(BAUDRATE); //Verbindung mit dem Sensor erstellen
@@ -29,43 +27,32 @@ void setup() {
   connect_WiFi(); //Mit WiFi verbinden (eigene Funktion am Ende definiert)
 }
 
-
-
 void loop() {
   bool validValue = true; //Boolean für die Überprüfung des Messwertes
   int co2value = sensor.getCO2(); //co2-Wert vom Sensor auslesen
   int temperature = sensor.getTemperature(); //Temperatur vom Sensor auslesen
   Serial.println(String(co2value) + "ppm"); //co2-Werte im Serial-Monitor ausgeben
-  Serial.println(String(temperature) + " C"); //Temperatur im Serial-Monitor ausgeben
+  //Serial.println(String(temperature) + " C"); //Temperatur im Serial-Monitor ausgeben
 
-  if (co2value == 0) { //Falls der Sensor den Wert 0 ausgibt, dann soll dieser nicht berücksichtigt werden
-    validValue = false;
-  }
-  if (temperature == 0) { //Falls der Sensor den Wert 0 ausgibt, dann soll dieser nicht berücksichtigt werden
+  if (co2value == 0 || temperature == 0) { //Falls der Sensor den Wert 0 ausgibt, dann soll dieser nicht berücksichtigt werden
     validValue = false;
   }
 
   if (validValue) {
-    ThingSpeak.setField( 1, co2value); //co2-Wert auf Field 1 eintragen
-    ThingSpeak.setField( 2, temperature); //Temperatur Wert auf Field 2 eintragen
-    ThingSpeak.writeFields(channelID, writeAPIKey); //Daten auf Thingspeak laden
+    ThingSpeak.setField(1, co2value); //co2-Wert auf Field 1 eintragen
+    ThingSpeak.setField(2, temperature); //Temperatur Wert auf Field 2 eintragen
+    ThingSpeak.writeFields(channelID, writeAPIkey); //Daten auf Thingspeak laden
   } else {
     Serial.println("last value ignored due to invalidity"); //Benutzer über die Umstände aufklären
   }
-  delay(15000); //15 Sekunden warten, bevor erneut ein Wert erfasst und gesendet wird
+  delay(30000); //30 Sekunden warten, bevor erneut ein Wert erfasst und gesendet wird
 }
 
 void connect_WiFi() {
   WiFi.begin(ssid, password); //Verbindung aufbauen
   while (WiFi.status() != WL_CONNECTED) { //Warten, bis eine Verbindung aufgebaut ist
-     delay(50);
-     Serial.println("/");
-     delay(50);
-     Serial.println("_");
-     delay(50);
-     Serial.println("\\");
-     delay(50);
-     Serial.println("|");
+     delay(500);
+     Serial.print(".");
   }
 
   Serial.println("Connected to WiFi"); //Bestätigung der Verbindung ausgeben
